f3b408af1fa7bff020bad1e8b0d4664b9f1dc388
Ali Elhefnawy <alihefnawey@gmail.com>
2025-01-21 19:40:03 +0800

    Initial commit

---
+++ lib/db/migrate.ts
@@ -0,0 +1,29 @@
+import { config } from 'dotenv';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/neon-http/migrator';
+import postgres from 'postgres';
+import { db } from './db'
+config({
+  path: '.env.local',
+});
+
+const runMigrate = async () => {
+  if (!process.env.POSTGRES_URL) {
+    throw new Error('POSTGRES_URL is not defined');
+  }
+
+  console.log('⏳ Running migrations...');
+
+  const start = Date.now();
+  await migrate(db, { migrationsFolder: './lib/db/migrations' });
+  const end = Date.now();
+
+  console.log('✅ Migrations completed in', end - start, 'ms');
+  process.exit(0);
+};
+
+runMigrate().catch((err) => {
+  console.error('❌ Migration failed');
+  console.error(err);
+  process.exit(1);
+});
